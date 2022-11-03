c579e1f8d2ee9586f6b544114927d27d8777dbce
prateek <113834812+testlearnprat@users.noreply.github.com>
2022-11-03 14:34:59 +0530

    Update tomcat_install.sh

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -9,9 +9,9 @@ cd
 rm -rf apache-tomcat*
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz
 tar -xvf apache-tomcat-9.0.68.tar.gz
-cp /home/ec2-user/t9/context.xml /home/ec2-user/apache-tomcat-9.0.68/webapps/manager/META-INF/context.xml
-cp /home/ec2-user/t9/context.xml /home/ec2-user/apache-tomcat-9.0.68/webapps/host-manager/META-INF/context.xml
-cp /home/ec2-user/t9/tomcat-users.xml /home/ec2-user/apache-tomcat-9.0.68/conf/
+cp /home/ec2-user/Install-Tomcat-/context.xml /home/ec2-user/apache-tomcat-9.0.68/webapps/manager/META-INF/context.xml
+cp /home/ec2-user/Install-Tomcat-/context.xml /home/ec2-user/apache-tomcat-9.0.68/webapps/host-manager/META-INF/context.xml
+cp /home/ec2-user/Install-Tomcat-/tomcat-users.xml /home/ec2-user/apache-tomcat-9.0.68/conf/
 cd
-sudo cp /home/ec2-user/t9/tomcat.service /etc/systemd/system/tomcat.service
+sudo cp /home/ec2-user/Install-Tomcat-/tomcat.service /etc/systemd/system/tomcat.service
 sh /home/ec2-user/apache-tomcat-9.0.68/bin/startup.sh
